8c28685bb26d9970b8e508699da90fbbb683b829
MajoLeon06 <majoleonpaz1321@gmail.com>
2026-02-22 11:54:38 -0600

    Datos y condición de validación

--- Ejercicio 2/Program.cs
+++ Ejercicio 2/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("ROL");
+﻿Console.WriteLine("Hora: (0-23)");
+int hora = int.Parse(Console.ReadLine());
+Console.WriteLine("Día: (1-7)");
+int day = int.Parse(Console.ReadLine());
+Console.WriteLine("ROL");
+Console.WriteLine("1:Estudiante\n2:Docente\n3:Técnico IT\n4:Visitante");
+Console.WriteLine("Ingrese opción:");
+int option = int.Parse(Console.ReadLine());
+if ((hora>=0 && hora<=23)&&(day>=0 && day<=7))
+{
+
+}
