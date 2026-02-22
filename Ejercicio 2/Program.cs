12dc25ff18b174af5ffd41c417f69a6eed1c9aa5
MajoLeon06 <majoleonpaz1321@gmail.com>
2026-02-22 12:09:38 -0600

    Case 1

--- Ejercicio 2/Program.cs
+++ Ejercicio 2/Program.cs
@@ -8,5 +8,43 @@ Console.WriteLine("Ingrese opción:");
 int option = int.Parse(Console.ReadLine());
 if ((hora>=0 && hora<=23)&&(day>=0 && day<=7))
 {
-
+    switch (option)
+    {
+        case 1:
+            {
+                Console.WriteLine("¿Carnet vigente? 1:Si 2:No");
+                int carnet = int.Parse(Console.ReadLine());
+                if (carnet==1 && (hora>=7 && hora<=18))
+                {
+                    Console.WriteLine("Acceso permitido");
+                }
+                else if (carnet==2 && (hora>=7 && hora<=18))
+                {
+                    Console.WriteLine("¿Tiene autorización? 1:Si 2:No");
+                    int autorizacion = int.Parse(Console.ReadLine());
+                    if (autorizacion==1)
+                    {
+                        Console.WriteLine("Acceso permitido");
+                    }
+                    else if (autorizacion==2)
+                    {
+                        Console.WriteLine("Acceso denegado");
+                    }
+                }
+                else if (hora<7 && hora>18)
+                {
+                    Console.WriteLine("¿Tiene autorización? 1:Si 2:No");
+                    int sino1 = int.Parse(Console.ReadLine());
+                    if (sino1==1)
+                    {
+                        Console.WriteLine("Acceso permitido");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Acceso denegado");
+                    }
+                }
+                break;
+            }
+    }
 }
